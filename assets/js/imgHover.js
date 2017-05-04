09ea63903780879a82ac6de2c369a7efc149a37a
Pedro Cruz <pedro@rokailabs.com>
2017-05-04 14:10:17 -0400

    Updated about us page

---
+++ assets/js/imgHover.js
@@ -0,0 +1,6 @@
+function hover(element) {
+    element.setAttribute('src', 'assets/img/team/profile-pc-alt.jpg');
+}
+function unhover(element) {
+    element.setAttribute('src', 'assets/img/team/profile-pc.jpg');
+}
